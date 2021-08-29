66c4598ea6f62ac0388c33797b0719fa55a2bae2
ShenShu2016 <shushen2013@gmail.com>
2021-08-29 16:32:03 -0400

    fix: update django admin

--- backend/news/admin.py
+++ backend/news/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from news.models import Topic, Article
+from news.models import Topic, Article, ArticleComment
 # Register your models here.
-
 
 admin.site.register(Topic)
 admin.site.register(Article)
+admin.site.register(ArticleComment)
